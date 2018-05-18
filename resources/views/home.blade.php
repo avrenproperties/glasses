fef92616559ccb8cc72bea76a0ff5755313af454
Pavel <dvdtygc@gmail.com>
2018-05-18 22:14:32 +0300

    Formatting, stick footer to the bottom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
 
 @section('content')
-    home view (:
+	home view (:
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
-    <title>
-        @if (isset($title))
-            $title
-        @else
-            Glasses
-        @endif
-    </title>
+	<meta charset="UTF-8">
+	<title>
+		@if (isset($title))
+			$title
+		@else
+			Glasses
+		@endif
+	</title>
 
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport"
+		  content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <meta name="csrf_token" content="{{ csrf_token() }}"/>
+	<meta name="csrf_token" content="{{ csrf_token() }}"/>
 
-    <link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.ico"/>
-    <link rel="icon" type="image/x-icon" href="images/icons/favicon.ico"/>
+	<link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/x-icon" href="images/icons/favicon.ico"/>
 
 	{!! \App\Http\Helpers\HTML::style('lib/bootstrap') !!}
-    {!! \App\Http\Helpers\HTML::style('layout') !!}
+	{!! \App\Http\Helpers\HTML::style('layout') !!}
 </head>
 <body>
 
@@ -34,15 +34,15 @@
 <div id="scripts">
 	{!! \App\Http\Helpers\HTML::script('lib/jquery') !!}
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-            integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-            crossorigin="anonymous">
-    </script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+			integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+			crossorigin="anonymous">
+	</script>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-            integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-            crossorigin="anonymous">
-    </script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+			integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+			crossorigin="anonymous">
+	</script>
 
 	{!! \App\Http\Helpers\HTML::script('app') !!}
 </div>
